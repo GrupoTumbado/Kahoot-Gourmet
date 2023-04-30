4d7d1659a90acd5f4e0f5d160ce09ed64fe3ad3b
Ian Ramirez-España <ianespana@users.noreply.github.com>
2023-04-30 14:13:06 -0600

    Actualizar nombres

--- Client/src/main/java/pw/espana/kahootgourmet/client/ClientController.java
+++ Client/src/main/java/pw/espana/kahootgourmet/client/ClientController.java
@@ -3,7 +3,7 @@ package pw.espana.kahootgourmet.client;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class ClientController {
     @FXML
     private Label welcomeText;
 
