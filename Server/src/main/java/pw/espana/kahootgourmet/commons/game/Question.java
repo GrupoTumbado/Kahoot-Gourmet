ae5f90ff8d45bd335870b1e55d8c60fc44d0c863
Ian Ramirez-España <ianespana@users.noreply.github.com>
2023-05-06 20:33:30 -0600

    Update Question.java

--- Server/src/main/java/pw/espana/kahootgourmet/commons/game/Question.java
+++ Server/src/main/java/pw/espana/kahootgourmet/commons/game/Question.java
@@ -1,5 +1,56 @@
 package pw.espana.kahootgourmet.commons.game;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 import java.io.Serializable;
 
-public record Question(String question, int scoreValue, Answer[] answers) implements Serializable {}
+public class Question implements Serializable, Cloneable {
+    private String question;
+    private int scoreValue;
+    private Answer[] answers;
+
+    public Question(String question, int scoreValue, Answer[] answers) {
+        this.question = question;
+        this.scoreValue = scoreValue;
+        this.answers = answers;
+    }
+
+    @Override
+    public Question clone() {
+        try {
+            return (Question) super.clone();
+        } catch (CloneNotSupportedException e) {
+            // should never happen since we implement Cloneable
+            throw new InternalError();
+        }
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public int getScoreValue() {
+        return scoreValue;
+    }
+
+    public void setScoreValue(int scoreValue) {
+        this.scoreValue = scoreValue;
+    }
+
+    public Answer[] getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(Answer[] answers) {
+        this.answers = answers;
+    }
+
+    public StringProperty questionProperty() {
+        return new SimpleStringProperty(getQuestion());
+    }
+}
